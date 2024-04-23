276181b5fddde2158beaffebbfb89cce88e5093a
JoffreDavid <jdariasb@uce.edu.ec>
2024-04-22 19:14:45 -0500

    Ejercicio de programacion numero 1

---
+++ Ejercicio001/src/Main/Main.java
@@ -0,0 +1,11 @@
+package Main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Figura figura1 = new Figura(new Circulo());
+		Figura figura2 = new Figura(new Rectangulo());
+		Figura figura3 = new Figura(new Triangulo());
+	}
+
+}
